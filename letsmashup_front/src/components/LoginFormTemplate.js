91dfb3eabfe4250c31f380cbef8265040a9050c7
최문정 <sgo1019363@mobmail.shinhan.com>
2024-01-25 14:08:51 +0900

    [컴포넌트생성_v3.0] (가입) 로그인 페이지 완료, 가입 페이지 생성 / 라우터 라이브러리 설치 및 적용

--- letsmashup_front/src/components/LoginFormTemplate.js
+++ letsmashup_front/src/components/LoginFormTemplate.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import logo_sns from "../image/logo_sns.png";
+import MainTemplate from "./MainTemplate";
+import HeaderTemplate from "./HeaderTemplate";
 
 const LoginFormBlock = styled.div`
     margin: 0 auto;
@@ -151,55 +153,90 @@ const LoginFormBlock = styled.div`
         background-position: -147px -49px;
         vertical-align: top;
     }
+
+    .txt_join_notice {
+        padding: 40px 0 14px;
+        font-weight: 500;
+        font-size: 13px;
+        text-align: center;
+
+        span {
+            text-decoration: underline;
+            color: #ff4800;
+        }
+    }
+
+    .btn_join {
+        display: block;
+        height: 56px;
+        border: 1px solid #5d5d5d;
+        border-radius: 28px;
+        box-sizing: border-box;
+        font-weight: 600;
+        font-size: 14px;
+        color: #1d1d1d;
+        text-align: center;
+        line-height: 56px;
+        text-decoration: none;
+    }
 `;
 
 function LoginFormTemplate() {
     return (
-        <LoginFormBlock>
-            <h2 className="title_login">로그인</h2>
-            <div className="form_div">
-                <form>
-                    <div className="form_id">
-                        <input placeholder="아이디 (이메일)" autoCapitalize="none" type="text" name="username"></input>
+        <MainTemplate>
+            <HeaderTemplate />
+            <LoginFormBlock>
+                <h2 className="title_login">로그인</h2>
+                <div className="form_div">
+                    <form>
+                        <div className="form_id">
+                            <input placeholder="아이디 (이메일)" autoCapitalize="none" type="text" name="username"></input>
+                        </div>
+                        <div className="form_pw">
+                            <input placeholder="비밀번호" autoCapitalize="none" type="password" name="password"></input>
+                        </div>
+                        <button className="btn_login" type="submit">
+                            로그인하기
+                        </button>
+                    </form>
+                    <div className="button_div">
+                        <h3>SNS 계정으로 로그인하기</h3>
+                        <div className="button_group">
+                            <div className="btn_kakao_div">
+                                <button className="btn_login_kakao" type="button">
+                                    <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path fillRule="evenodd" clipRule="evenodd" d="M9 0C4.0294 0 0 3.09858 0 6.92081C0 9.39196 1.68456 11.5603 4.21858 12.7847C4.08072 13.2484 3.33268 15.7676 3.30291 15.9656C3.30291 15.9656 3.285 16.1144 3.38374 16.1711C3.48248 16.2277 3.59862 16.1837 3.59862 16.1837C3.88177 16.1452 6.88214 14.0897 7.40137 13.7327C7.92017 13.8044 8.45446 13.8416 9 13.8416C13.9706 13.8416 18 10.7431 18 6.92081C18 3.09858 13.9706 0 9 0Z" fill="black"></path>
+                                    </svg>
+                                    <span className="btn_kakao_txt">빠른 시작</span>
+                                    <span className="btn_kakao_txt2 btntxt">카카오로 로그인하기</span>
+                                </button>
+                            </div>
+                            <div className="btn_naver_div btn_icon">
+                                <button className="btn_login_naver">
+                                    <span className="btntxt">네이버로 로그인하기</span>
+                                </button>
+                            </div>
+                            <div className="btn_facebook_div btn_icon">
+                                <button className="btn_login_facebook">
+                                    <span className="btntxt">페이스북으로 로그인하기</span>
+                                </button>
+                            </div>
+                            <div className="btn_apple_div btn_icon">
+                                <button className="btn_login_apple">
+                                    <span className="btntxt">Apple로 로그인하기</span>
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                    <div className="form_pw">
-                        <input placeholder="비밀번호" autoCapitalize="none" type="password" name="password"></input>
-                    </div>
-                    <button className="btn_login" type="submit">
-                        로그인하기
-                    </button>
-                </form>
-                <div className="button_div">
-                    <h3>SNS 계정으로 로그인하기</h3>
-                    <div className="button_group">
-                        <div className="btn_kakao_div">
-                            <button className="btn_login_kakao" type="button">
-                                <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M9 0C4.0294 0 0 3.09858 0 6.92081C0 9.39196 1.68456 11.5603 4.21858 12.7847C4.08072 13.2484 3.33268 15.7676 3.30291 15.9656C3.30291 15.9656 3.285 16.1144 3.38374 16.1711C3.48248 16.2277 3.59862 16.1837 3.59862 16.1837C3.88177 16.1452 6.88214 14.0897 7.40137 13.7327C7.92017 13.8044 8.45446 13.8416 9 13.8416C13.9706 13.8416 18 10.7431 18 6.92081C18 3.09858 13.9706 0 9 0Z" fill="black"></path>
-                                </svg>
-                                <span className="btn_kakao_txt">빠른 시작</span>
-                                <span className="btn_kakao_txt2 btntxt">카카오로 로그인하기</span>
-                            </button>
-                        </div>
-                        <div className="btn_naver_div btn_icon">
-                            <button className="btn_login_naver">
-                                <span className="btntxt">네이버로 로그인하기</span>
-                            </button>
-                        </div>
-                        <div className="btn_facebook_div btn_icon">
-                            <button className="btn_login_facebook">
-                                <span className="btntxt">페이스북으로 로그인하기</span>
-                            </button>
-                        </div>
-                        <div className="btn_apple_div btn_icon">
-                            <button className="btn_login_apple">
-                                <span className="btntxt">Apple로 로그인하기</span>
-                            </button>
-                        </div>
-                    </div>
+                    <p className="txt_join_notice">
+                        회원가입하고 <span>다양한 상품</span>을 살펴보세요!
+                    </p>
+                    <a className="btn_join" href="/join">
+                        간편 회원가입하기
+                    </a>
                 </div>
-            </div>
-        </LoginFormBlock>
+            </LoginFormBlock>
+        </MainTemplate>
     );
 }
 
