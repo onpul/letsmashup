d31cb5956a4d0e4deb0c3a475cf931a8781b6ff7
vvvv1352@naver.com <vvvv1352@naver.com>
2024-01-27 09:01:46 +0900

    소스트리 연동 테스트

--- letsmashup_back/src/main/java/com/example/letsmashup_back/LetsmashupBackApplication.java
+++ letsmashup_back/src/main/java/com/example/letsmashup_back/LetsmashupBackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LetsmashupBackApplication {
 
     public static void main(String[] args) {
+        System.out.println("테스트?");
         SpringApplication.run(LetsmashupBackApplication.class, args);
     }
 
