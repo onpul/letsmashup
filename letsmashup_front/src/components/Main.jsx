72cd81177d42933a6723e321900a4735f5773b6a
최문정 <sgo1019363@mobmail.shinhan.com>
2024-01-29 16:35:14 +0900

    [기능구현_v2.2] 메인 화면에 API 테스트용 버튼 구현

--- letsmashup_front/src/components/Main.jsx
+++ letsmashup_front/src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import MainTemplate from "./MainTemplate";
 import styled from "styled-components";
 import BtnDefault from "./BtnDefault";
@@ -9,43 +9,89 @@ const MainBlock = styled.div`
     border-top: 2px solid rgb(244, 244, 244);
     position: relative;
     flex: 1 0 100%;
+    text-align: center;
+    margin-bottom: 60px;
 
-    h3 {
-        font-weight: 500;
-        font-size: 16px;
+    h1 {
+        font-weight: 700;
+        font-size: 18px;
         text-align: center;
-        color: #000000;
+        color: blue;
         margin-bottom: 15px;
         line-height: 24px;
+    }
+
+    h3 {
+        margin-top: 20px;
+        font-size: 16px;
+    }
+
+    .apiInfo {
+        text-align: left;
+    }
+    .apiRes {
+        color: red;
+        margin-top: 20px;
     }
 `;
 
 function Main() {
 
-    const fncSendApi = () => {
-    axios.post("http://localhost:8080/api/v1/member", {
-        username: "test@test.com",
-        password: "testPassword!",
-    }).catch((error) => {
-        // 오류 발생 시 실행
-        console.log(">>> error : " + error + " <<<");
-    }).then((response) => {
-        // 항상 실행
-        console.log(">>> response : " + response + " <<<");
-    });
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+
+    const fncSendApi = (e) => {
+        const sURL = e.target.title;
+        console.log(">>> sURL : " + sURL + " <<<");
+        axios.post(sURL, {
+            username: "test@test.com",
+            password: "testPassword!",
+        }).catch((error) => {
+            // 오류 발생 시 실행
+            setError(error + " ");
+            console.log(">>> error : " + error + " <<<");
+        }).then((response) => {
+            // 항상 실행
+            setMessage(response + " ");
+            console.log(">>> response : " + response + " <<<");
+        });
     }
     return (
         <MainTemplate>
             <MainBlock>
                 <div>
-                    <h3>임시 메인 페이지</h3>
+                    <h1>▶️ 임시 메인 페이지 ◀️</h1>
                     <BtnDefault title="가입하기 페이지로 이동하기" href="/join" />
                     <BtnDefault title="로그인 페이지로 이동하기" href="/login" />
                     <br />
                     <hr />
                     <br />
-                    <h3>API 호출 테스트</h3>
-                    <BtnDefault title="/api/v1/member" onClick={fncSendApi}/>
+                    <h1>▶️ API 호출 테스트 ◀️</h1>
+                    <h3>회원가입 POST</h3>
+                    <BtnDefault title="/api/v1/member" onClick={fncSendApi} />
+                    <h3>로그인 POST</h3>
+                    <BtnDefault title="/api/v1/memberLogin" onClick={fncSendApi} />
+                    <h3>로그아웃 PUT</h3>
+                    <BtnDefault title="/api/v1/memberLogout" onClick={fncSendApi} />
+                    <h3>아이디찾기 GET</h3>
+                    <BtnDefault title="/api/v1/memberIdFind" onClick={fncSendApi} />
+                    <h3>비밀번호찾기 GET</h3>
+                    <BtnDefault title="/api/v1/memberPassFind" onClick={fncSendApi} />
+                    <br />
+                    <hr />
+                    <br />
+                    <h1>▶️ API 호출 정보 ◀️</h1>
+                    <div className="apiInfo">
+                        <div className="apiRes">
+                            <b>error 결과 :</b>
+                        </div>
+                        <span>{error}</span>
+                        <div className="apiRes">
+                            <b>message 결과 :</b>
+                        </div>
+                        <span>{message}</span>
+                    </div>
+                    <hr/>
                 </div>
             </MainBlock>
         </MainTemplate>
